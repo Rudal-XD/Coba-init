6500e063e1d4eb2c542accb4e67e3b36f16611b7
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2022-12-02 00:14:34 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,21 +1,29 @@
-import os,sys,json,requests
+import requests, json, time, re, os
 
-def __init__(self):
-	print(f'1.open')
-	
-	def pilih(self):
-		masuk = input('choose:')
-		print()
-		if masuk in ['']:
-			print()
-		elif masuk in ['1']:
-			try:
-				r=requests.get('https://google.com')
-			except IOError:
-				print('error')
-		elif masuk in ['2']:
-		 	print()
-		 	exit()
-		 	
+# Warna
+KUNING = ('\x1b[1;93m')
+MERAH = ('\x1b[1;91m')
+HIJAU = ('\x1b[1;92m')
+PUTIH = ('\x1b[1;97m')
+# Banner
+banner = (f"""     {MERAH}({PUTIH}-Rudal-XD-{MERAH}){PUTIH}
+{MERAH}╔═╗┌─┐┌┐┌┬  ┬┌─┐┬─┐┌┬┐
+{MERAH}║  │ ││││└┐┌┘├┤ ├┬┘ │
+{PUTIH}╚═╝└─┘┘└┘ └┘ └─┘┴└─ ┴
+""")
+# Convert Cookie Ke Token
+class convert:
+
+  def __init__(self):
+    os.system('clear')
+    print(f"""{banner}
+{HIJAU}1.{PUTIH} Mendapatkan token EAAI
+{HIJAU}2.{PUTIH} Dapatkan token EAAB
+{HIJAU}3.{PUTIH} Cara menggunakan
+{HIJAU}4.{PUTIH} Keluar {HIJAU}({MERAH}exit{HIJAU}){MERAH}
+   """)
+    masuk = input(f"{KUNING}?.{PUTIH} Choose :{HIJAU} ")
+    if masuk == '1' or masuk == '01':
+
 if __name__=='__main__':
-	os.system('git pull');__init__('self')
+  os.system('git pull');convert()
